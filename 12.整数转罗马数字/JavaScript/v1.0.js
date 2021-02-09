393dd409ccb00107e0ff3782673aabeb4368c79f
luqingliang <luqingliang1996@foxmail.com>
2021-02-09 12:00:33 +0800

    贪心算法，从大到小

---
+++ 12.整数转罗马数字/JavaScript/v1.0.js
@@ -0,0 +1,17 @@
+/**
+ * 贪心算法，从大到小，使用for比纯while快
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function(num) {
+    let keys = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1],
+        values = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+    let res = "";
+    for (let i = 0; i < keys.length; i++) {
+        while (num >= keys[i]) {
+            num -= keys[i];
+            res += values[i];
+        }
+    }
+    return res;
+};
